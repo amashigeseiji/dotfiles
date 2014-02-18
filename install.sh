5d6b94160f2de080a32a785f57f88851579be842
amashigeseiji <amashige@tejimaya.com>
2014-02-18 09:50:48 +0900

    インストール用スクリプトを修正

--- install.sh
+++ install.sh
@@ -1,12 +1,85 @@
 #!/bin/sh
 path=`echo $(cd $(dirname $0);pwd)`
-if [ ${#} -eq 0 ];then
-  echo 'arguments:
-    zsh
-    vim
-    screen
-    tmux'
+args=($(printf '%s\n' $@ | egrep -v '^-.*'))
+dirs=($(find $path -name '.install' | awk -F/ '{print $(NF-1)}'))
+
+if [ $# -eq 0 ];then
+  printf '%s\n' 'arguments:'
+  printf '  %s\n' ${dirs[@]}
   exit
 fi
 
-for i in ${@}; do sh $path/$i/install.sh; done
+linkto() {
+  local path=$1
+  local filename=$2
+  local prefix=''
+  if [ $3 ];then
+    prefix=$3;
+  fi
+  if [ -L ~/${prefix}${filename} ];then
+    ln -snf $path/${filename} ~/${prefix}${filename}
+    echo 'linked ~/'${prefix}${filename}
+  else
+    if [ -e ~/${prefix}${filename} ];then
+      mv ~/${prefix}${filename} ~/${prefix}${filename}.bak
+      echo 'moved ~/'${prefix}${filename}.bak
+    fi
+    ln -s $path/${filename} ~/${prefix}${filename}
+    echo 'linked ~/'${prefix}${filename}
+  fi
+}
+
+vim_install() {
+  linkto $1 'vimrc' '.'
+  linkto $1 'gvimrc' '.'
+  if [ ! -d ~/.vim ];then
+    mkdir ~/.vim
+    echo 'mkdir ~/.vim'
+  fi
+  linkto $1 'colors' '.vim/'
+  linkto $1 'plugin' '.vim/'
+  if [ ! -d ~/.vim/bundle/vundle ];then
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    echo 'clone bundle ~/.vim/bundle/vundle'
+  fi
+  echo 'finished vim install'
+}
+
+zsh_install() {
+  linkto $1 'zsh' '.'
+  if [ ! -e ~/.zshrc ];then
+    touch ~/.zshrc
+    echo 'targetPlugins=("prompt")' >> ~/.zshrc
+    echo 'prompt_color="yellow"' >> ~/.zshrc
+    echo 'source ~/.zsh/zshrc-mac' >> ~/.zshrc
+    echo 'created ~/.zshrc'
+  fi
+  echo 'finished zsh install'
+}
+
+tmux_install() {
+  linkto $1 'tmux.conf' '.'
+  if [ ! -d ~/.tmux ];then
+    mkdir ~/.tmux
+    echo 'mkdir ~/.tmux'
+  fi
+  linkto $1 'bin' '.tmux/'
+  echo 'finished tmux install'
+}
+
+screen_install() {
+  local path=$path/screen
+  linkto $path 'screenrc' '.'
+  echo 'finished screen install'
+}
+
+for i in ${args[@]};
+do
+  if [ ! -d $path/$i ];then
+    echo $i' is not exist.';
+  elif [ ! -e $path/$i/.install ];then
+    echo $i' has no .install';
+  else
+    eval $i'_install '$path'/'$i
+  fi
+done
--- screen/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path=`echo $(cd $(dirname $0);pwd)`
-if [ ! -e ~/.screenrc ];then
-  ln -s $path/screenrc ~/.screenrc
-  echo 'linked ~/.screenrc'
-fi
-echo 'finished screen install'
