206003117785ca9b80a8c4742fa566929a56cdd7
amashige <tenjuu99@gmail.com>
2015-04-06 11:17:47 +0900

    cakeの補完用コマンドを修正

--- zsh/plugins/cake.zsh
+++ zsh/plugins/cake.zsh
@@ -13,13 +13,14 @@ function cake () {
 _cake() {
     __get_values () { echo $(exec cake completion commands) }
     __get_subcommand_values () { echo $(exec cake completion subcommands ${1}) }
+    __get_options () { echo $(exec cake completion options ${1}) }
 
     _arguments : \
       "1:commands:(($(__get_values)))" \
-      "2:subcommands:(($(__get_subcommand_values ${words[2]})))" \
+      "2:subcommands:(($(__get_subcommand_values ${words[2]}) $(__get_options ${words[2]})))" \
       && return 0
 
-    unfunction __get_values __get_subcommand_values
+    unfunction __get_values __get_subcommand_values __get_options
 }
 
 compdef _cake cake
