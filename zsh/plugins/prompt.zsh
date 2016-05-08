ad23ce3a87e68dd37ad932542aab8842c5641f3c
amashigeseiji <tenjuu99@gmail.com>
2016-05-08 16:12:35 +0900

    modified zsh

--- zsh/plugins/prompt.zsh
+++ zsh/plugins/prompt.zsh
@@ -5,9 +5,23 @@ fi
 if [ -z ${prompt_name} ];then
   prompt_name='%m'
 fi
+
 PROMPT="%B%F{red}@%f%F{${prompt_color}}${prompt_name}%f %F{blue}>>%f%b "
 PROMPT2="%F{blue}%_> %f"
 SPROMPT='correct: %F{red}'%R'%f to %F{blue}'%r'%f [y,n,a,e]? %f'
+
+function rprompt_git_off() {
+  RPROMPT='%B%F{blue}[%f%F{blue}%~]%f%b'
+}
+
+function rprompt_git_on() {
+  [ ${prompt_git_use} ] && RPROMPT='%B%F{blue}[%f%b`rprompt-git-current-branch`%B%F{blue}%~]%f%b' || rprompt_git_off
+}
+
+function rprompt_git_nostatus() {
+  rprompt_nostatus=1
+}
+
 if [ ${prompt_git_use} ];then
   function rprompt-git-current-branch { #{{{
     # Show branch name in Zsh's right prompt(for git)
@@ -22,10 +36,7 @@ if [ ${prompt_git_use} ];then
     gitdir=`git rev-parse --git-dir 2> /dev/null`
     action=`VCS_INFO_git_getaction "$gitdir"` && action="($action)"
 
-    if [[ -e "$gitdir/rprompt-nostatus" ]]; then
-      echo "$name$action "
-      return
-    fi
+    [ $rprompt_nostatus ] && echo "${name}${action} " && return
 
     st=`git status 2> /dev/null`
     if [[ "$st" =~ "(?m)^nothing to" ]]; then
@@ -41,7 +52,7 @@ if [ ${prompt_git_use} ];then
     echo "$color$name$action%f%b "
   }
   #}}}
-  RPROMPT='%B%F{blue}[%f%b`rprompt-git-current-branch`%B%F{blue}%~]%f%b'
+  rprompt_git_on
 else
-  RPROMPT='%B%F{blue}[%f%F{blue}%~]%f%b'
+  rprompt_git_off
 fi
