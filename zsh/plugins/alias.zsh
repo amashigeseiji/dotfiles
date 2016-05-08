3ec7173a1950bca0b93059e8b0a65cf0bf71a921
amashigeseiji <tenjuu99@gmail.com>
2016-05-09 02:18:31 +0900

    modified anyenv.zsh
    anyenv が重くてシェルの起動に時間がかかっていたので後から起動できるようにする

--- zsh/plugins/alias.zsh
+++ zsh/plugins/alias.zsh
@@ -2,19 +2,16 @@ function makeAlias(){
   local IFS_BACKUP apps appname
   IFS_BACKUP=$IFS
   IFS=$'\n'
-  apps=(`find $1 -maxdepth 1 -name "*app"`)
+  apps=(`find $1 -maxdepth 3 -name "*app" -exec basename {} .app ';'`)
   for i in ${apps[@]}
   do
-    appname=$(echo $i | grep -oE '[a-zA-Z0-9 ]+(\.app)+$' | sed -e "s/.app//g" | sed -e "s/ //g" | sed -e "s/Microsoft//g" | sed -e "s/Adobe//g")
-    if [ 'boot2docker' = ${appname} ];then
-      continue;
-    fi
+    appname=${i// /}
+    appname=${appname#Microsoft}
+    appname=${appname#Adobe}
+    [ 'boot2docker' = ${appname} ] && continue
     alias ${appname}="open -a \"$i\""
   done
   IFS=${IFS_BACKUP}
 }
 makeAlias '/Applications'
-if [ -e '/Applications/Microsoft Office 2011/' ];then
-  makeAlias '/Applications/Microsoft Office 2011/'
-fi
 unfunction makeAlias
--- zsh/plugins/anyenv.zsh
+++ zsh/plugins/anyenv.zsh
@@ -1,9 +1,43 @@
 if [ ! -e $HOME/.anyenv ];then
   git clone https://github.com/riywo/anyenv ~/.anyenv
 fi
+
 export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
-for D in `ls $HOME/.anyenv/envs`
+export ANYENV_anyenv=
+
+for i in $(ls $HOME/.anyenv/envs/)
 do
-    export PATH="$HOME/.anyenv/envs/$D/shims:$PATH"
+  export ANYENV_${i%/}=
 done
+
+source $HOME/.anyenv/completions/anyenv.${SHELL##*/}
+
+function anyenv_init() {
+  local env='anyenv'
+  local export_var_prefix=ANYENV_
+  [ $# -eq 1 ] && {
+    [[ $1 =~ .*env ]] && env=$1 || return 1
+  }
+
+  local export_var=${export_var_prefix}${env}
+  [ ! "$(eval printf '${'$export_var':-UNDEF}')" = 'UNDEF' ] && return
+
+  [ $env = 'anyenv' ] && {
+    eval "$(anyenv init - ${SHELL})"
+    export ${export_var}=1
+    for dir in `ls $HOME/.anyenv/envs`
+    do
+      dir=${dir%/}
+      export PATH="$HOME/.anyenv/envs/$dir/shims:$PATH"
+      export_var=${export_var_prefix}${dir}
+      export $export_var=1
+      [ -e $HOME/.anyenv/envs/$dir/completions/$dir.${SHELL##*/} ] && source $HOME/.anyenv/envs/$dir/completions/$dir.${SHELL##*/}
+    done
+  } || {
+    export ${env:u}_ROOT="$HOME/.anyenv/envs/$env"
+    export PATH="$HOME/.anyenv/envs/$env/bin:$HOME/.anyenv/envs/$env/shims:$PATH"
+    eval "$($env init - ${SHELL})"
+    export ${export_var}=1
+    [ -e $HOME/.anyenv/envs/$env/completions/$env.${SHELL##*/} ] && source $HOME/.anyenv/envs/$env/completions/$env.${SHELL##*/}
+  }
+}
